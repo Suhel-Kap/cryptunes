0a3f1cb0abdca3df525c96de631783884da2500c
Suhel Kapadia <suhelkapadia2@gmail.com>
2023-03-08 12:39:15 +0530

    user page also done

---
+++ pages/api/getUserNfts.ts
@@ -0,0 +1,37 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import {ethers} from "ethers";
+import {cryptunesAbi, cryptunesAddress} from "../../constants";
+import {intNumberFromHexString} from "@coinbase/wallet-sdk/dist/util";
+
+const getPrivateKey = () => {
+    return process.env.PRIVATE_KEY
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<string>
+) {
+    const {address} = req.body
+    console.log("address:", address)
+    const wallet = new ethers.Wallet(getPrivateKey()!)
+    const provider = new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/fantom_testnet');
+    const signer = provider.getSigner(wallet.address)
+    const contract = new ethers.Contract(cryptunesAddress["cryptunesAddress"], cryptunesAbi, signer)
+    const totalNfts = intNumberFromHexString((await contract.totalSupply())._hex)
+    console.log("totalNfts:", totalNfts)
+    let nftData = []
+    for (let i = 0; i < totalNfts; i++) {
+        const nft = await contract.getToken(i)
+        if (nft[2].toLowerCase() === address) {
+            let data_ = {price: "", tokenId: 0, availableTokens: 0, maxCap: 0, metadata: {}}
+            const ipfsData = await (await fetch(nft.metadataURL)).json()
+            data_.price = ethers.utils.formatEther(nft[5])
+            data_.tokenId = i
+            data_.availableTokens = intNumberFromHexString(nft[4]._hex)
+            data_.maxCap = intNumberFromHexString(nft[3]._hex)
+            data_.metadata = {...ipfsData}
+            nftData.push(data_)
+        }
+    }
+    res.status(200).json(JSON.stringify(nftData))
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Layout from "@/components/Layout";
 import Connect from "@/components/Conenct";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -12,8 +13,27 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
         <Layout>
-            <Connect />
-            <p>Hello</p>
+            <div className="flex flex-col justify-center items-center w-full h-fit absolute top-11">
+                <h1 className="text-4xl font-bold text-center text-slate-600 my-4">
+                    <span className="text-purple-700">Cryptunes</span> is a dynamic NFT platform
+                </h1>
+                <p className="text-xl text-center text-slate-400 my-4">
+                    Create your own NFTs and sell them on the marketplace
+                </p>
+                <div className="btn-group my-4">
+                    <button className="btn btn-primary btn-lg">
+                        <Link href={"https://github.com/Suhel-Kap/cryptunes"}>
+                            <a target={"_blank"}>Source Code</a>
+                        </Link>
+                    </button>
+                    <button className="btn btn-secondary btn-lg">
+                        <Link href={"/create"}>
+                            <a>Create NFT</a>
+                        </Link>
+                    </button>
+                </div>
+
+            </div>
         </Layout>
     </>
   )
