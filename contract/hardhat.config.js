ca0948b1fbcee84ba3a8547738ead30ddd08f8b5
Nick Lionis <nicklioniss@gmail.com>
2023-03-08 12:41:03 +0200

    hardhat folder

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -18,12 +18,12 @@ module.exports = {
         ftmTestnet: {
             url: `https://rpc.testnet.fantom.network`,
             chainId: 4002,
-            accounts: [`0x304030045e69a283fbc39a3f1b7b281ede03afdefa6f480a682e8974bc20b547`]
+            accounts: [``]
         }
     },
     etherscan: {
         apiKey: {
-            ftmTestnet: `S5FUSTGRR4TJU5TIXUXKP2XRABXJZBSP5W`,
+            ftmTestnet: ``,
         }
     }
 };
